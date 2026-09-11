5485c1ed4941dd047c0e82b402b3da0308bd64cb
hans-cmd-s <hanshjelmeland@gmail.com>
2026-09-11 18:30:05 +0200

    Add project files.

---
+++ 3-Semester HV prosjekt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _3_Semester_HV_prosjekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
