1b565ffd43171f07e1b033b394f868b745d26d56
hans-cmd-s <hanshjelmeland@gmail.com>
2026-09-22 22:49:41 +0200

    Updates to DataAccess including DB factory etc. some updates to controllersUpdates to DataAccess and Controllers.

--- 3-Semester HV prosjekt/Models/Entities/Resource.cs
+++ 3-Semester HV prosjekt/Models/Entities/Resource.cs
@@ -1,14 +1,13 @@
-﻿namespace _3_Semester_HV_prosjekt.Models.Entities
+﻿namespace _3_Semester_HV_prosjekt.Models.Entities;
+
+public class Resource
 {
-    public class Resource
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public string Type { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
-        public string City { get; set; } = string.Empty;
-        public string ZipCode { get; set; } = string.Empty;
-        public string OwnerTelephone { get; set; } = string.Empty;
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
+    public string OwnerTelephone { get; set; } = string.Empty;
 }
